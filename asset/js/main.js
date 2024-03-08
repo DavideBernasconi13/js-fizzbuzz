c9c9d9e00a33d266839ac261e11ebdab1c031ef9
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-03-08 14:48:48 +0100

    templateHTML in Javascript e css

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,14 +2,40 @@
 // ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-for (i = 1; i <= 100; i++) {
+
+// crea un div 
+const wrapper = document.createElement('div');
+
+// aggiungi le classi 
+wrapper.className = 'd-flex justify-content-between align-content-center flex-wrap';
+console.log(wrapper);
+// aggiungi un ID 
+wrapper.setAttribute('Id', 'main-box');
+console.dir(wrapper);
+
+// determina il numero massimo di celle
+let maxBox = 100
+// dichiaro il template html vuoto
+let tmpHtml = '';
+
+// ciclo for 
+for (i = 1; i <= maxBox; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
+        tmpHtml += ` <div class="box">${"FizzBuzz"}</div>`;
+        //console.log("FizzBuzz");
     } else if (i % 3 === 0) {
-        console.log("Fizz");
+        tmpHtml += ` <div class="box">${"Fizz"}</div>`;
+        //console.log("Fizz");
     } else if (i % 5 === 0) {
-        console.log("Buzz");
+        tmpHtml += ` <div class="box buzz">${"Buzz"}</div>`;
+        //console.log("Buzz");
     } else {
-        console.log(i);
+        tmpHtml += ` <div class="box">${i}</div>`;
+        //console.log(i);
     }
 }
+
+wrapper.innerHTML = tmpHtml;
+
+const container = document.querySelector('.container');
+container.append(wrapper);
